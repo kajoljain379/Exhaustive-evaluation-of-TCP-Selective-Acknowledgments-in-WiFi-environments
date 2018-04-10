4d5d04cf6ec70f23c9d9b21c2a127dbf16bb0b67
Arnab <pal_arnab23@yahoo.co.in>
2018-04-11 04:25:08 +0530

    Changed Propagation Loss Model to Jakes

--- ns-3.27/scratch/error-model2.cc
+++ ns-3.27/scratch/error-model2.cc
@@ -33,76 +33,48 @@
 // Default Network Topology
 //
 //   Wifi 10.1.3.0
-//                 AP
-//  *    *    *    *
-//  |    |    |    |    10.1.1.0
-// n5   n6   n7   n0 -------------- n1   n2   n3   n4
-//                   point-to-point  |    |    |    |
-//                                   ================
-//                                     LAN 10.1.2.0
+//       AP
+//  *    *
+//  |    |    10.1.1.0
+// n2   n0 -------------- n1
+//         point-to-point
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("ThirdScriptExample");
+NS_LOG_COMPONENT_DEFINE ("SACK Evaluation");
 
-Ptr<PacketSink> sinkptr;                       /* Pointer to the packet sink application */
-uint64_t lastTotalRx = 0;                     /* The value of the last total received bytes */
 uint64_t dropped = 0;
+//uint64_t dropped1 = 0;
 
 
-void Receive (Ptr<const Packet> p/*, double snr, WifiTxVector txVector*/)
+void RxDrop (Ptr<const Packet> p)
 {
-  //std::cout << "RxDrop at " << Simulator::Now ().GetSeconds () << std::endl;
   dropped++;
 }
 
-
-void
-CalculateThroughput ()
+/*void RxDrop1 (Ptr<const Packet> p)
 {
-   //Time now = Simulator::Now ();                                         /* Return the simulator's virtual time. */
-   //double cur = (sinkptr->GetTotalRx () - lastTotalRx) * (double) 8 / 1e5;      /*Convert Application RX Packets to MBits. */
-   //std::cout << now.GetSeconds () << "s: \t" << cur << " Mbit/s" << std::endl;
-   //lastTotalRx = sinkptr->GetTotalRx ();
-   Simulator::Schedule (MilliSeconds (100), &CalculateThroughput);
-}
-
-static void
-RxDrop (Ptr<const Packet> p)
-{
-  std::cout << "RxDrop at " << Simulator::Now ().GetSeconds () << std::endl;
-}
+  dropped1++;
+}*/
 
 int 
 main (int argc, char *argv[])
 {
-  bool verbose = true;
-  //uint32_t nCsma = 3;
   uint32_t nWifi = 1;
-  bool tracing = true;
+  bool tracing = false;
+  bool en_sack = true;
 
   CommandLine cmd;
   //cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
   cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
-  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
   cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
+  cmd.AddValue ("sack", "Enable Selective ACKnowledgements", en_sack);
 
   cmd.Parse (argc,argv);
 
   // The underlying restriction of 18 is due to the grid position
   // allocator's configuration; the grid layout will exceed the
   // bounding box if more than 18 nodes are provided.
-  if (nWifi > 18)
-    {
-      std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box" << std::endl;
-      return 1;
-    }
-
-  if (verbose)
-    {
-      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    }
 
   NodeContainer p2pNodes;
   p2pNodes.Create (2);
@@ -131,10 +103,8 @@ main (int argc, char *argv[])
   YansWifiPhyHelper phyAP = YansWifiPhyHelper::Default ();
   YansWifiPhyHelper phyST = YansWifiPhyHelper::Default ();
 
-  //YansWifiPhy wap = GetObject(phyAP.GetTypeId());
-
-  phyAP.Set ("TxGain", DoubleValue (55)); //was 25
-  phyAP.Set ("RxGain", DoubleValue (90)); //was 90
+  phyAP.Set ("TxGain", DoubleValue (55));
+  phyAP.Set ("RxGain", DoubleValue (90));
   phyST.Set("RxGain", DoubleValue (90));
   phyAP.SetChannel (channel);
   phyST.SetChannel (channel);
@@ -142,11 +112,9 @@ main (int argc, char *argv[])
   phyAP.Set ("TxPowerStart", DoubleValue (20.0));
   phyAP.Set ("TxPowerEnd", DoubleValue (20.0));
   phyAP.Set ("TxPowerLevels", UintegerValue (1));
-  //phy.Set ("TxGain", DoubleValue (0));
-  //phy.Set ("RxGain", DoubleValue (0));
   phyST.Set ("RxNoiseFigure", DoubleValue(30.0));
   phyST.Set ("CcaMode1Threshold", DoubleValue (30.0));
-  phyST.Set ("EnergyDetectionThreshold", DoubleValue (35));
+  phyST.Set ("EnergyDetectionThreshold", DoubleValue (55.0));
   phyAP.SetErrorRateModel ("ns3::YansErrorRateModel");
   phyST.SetErrorRateModel ("ns3::YansErrorRateModel");
 
@@ -159,7 +127,7 @@ main (int argc, char *argv[])
   Ssid ssid = Ssid ("ns-3-ssid");
   mac.SetType ("ns3::StaWifiMac",
                "Ssid", SsidValue (ssid),
-               "ActiveProbing", BooleanValue (false));
+               "ActiveProbing", BooleanValue (true));
 
   NetDeviceContainer staDevices;
   staDevices = wifi.Install (phyST, mac, wifiStaNodes);
@@ -176,8 +144,13 @@ main (int argc, char *argv[])
   Ptr<WifiPhy> wp = wnd->GetPhy ();
   Ptr<YansWifiPhy> phySta = wp->GetObject<YansWifiPhy> ();
 
-  //phySta->SetReceiveOkCallback (MakeCallback (&Receive));
-  phySta->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback(&Receive));
+  //Ptr<WifiNetDevice> wnd1 = apDevices.Get (0)->GetObject<WifiNetDevice> ();
+  //Ptr<WifiPhy> wp1 = wnd1->GetPhy ();
+  //Ptr<YansWifiPhy> phySta1 = wp1->GetObject<YansWifiPhy> ();
+
+  phySta->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback(&RxDrop));
+
+  //phySta1->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback(&RxDrop1));
 
 
   MobilityHelper mobility;
@@ -185,7 +158,7 @@ main (int argc, char *argv[])
   mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
                                  "MinX", DoubleValue (0.0),
                                  "MinY", DoubleValue (0.0),
-                                 "DeltaX", DoubleValue (15.0),
+                                 "DeltaX", DoubleValue (100.0),
                                  "DeltaY", DoubleValue (10.0),
                                  "GridWidth", UintegerValue (3),
                                  "LayoutType", StringValue ("RowFirst"));
@@ -200,7 +173,6 @@ main (int argc, char *argv[])
   mobility.Install (p2pNodes.Get (1));
 
   InternetStackHelper stack;
-  //stack.Install (csmaNodes);
   stack.Install (p2pNodes.Get (1));
   stack.Install (wifiApNode);
   stack.Install (wifiStaNodes);
@@ -211,26 +183,19 @@ main (int argc, char *argv[])
   Ipv4InterfaceContainer p2pInterfaces, stInterface;
   p2pInterfaces = address.Assign (p2pDevices);
 
-  //address.SetBase ("10.1.2.0", "255.255.255.0");
-  //Ipv4InterfaceContainer csmaInterfaces;
-  //csmaInterfaces = address.Assign (csmaDevices);
-
   address.SetBase ("10.1.3.0", "255.255.255.0");
   stInterface = address.Assign (staDevices);
   address.Assign (apDevices);
 
-  Config::SetDefault ("ns3::TcpSocketBase::Sack", BooleanValue (true));
+  Config::SetDefault ("ns3::TcpSocketBase::Sack", BooleanValue (en_sack));
 
-  //UdpEchoServerHelper echoServer (9);
   uint16_t sinkPort = 8080;
   Address sinkAddress (InetSocketAddress (stInterface.GetAddress (0),sinkPort));
   PacketSinkHelper sink ("ns3::TcpSocketFactory",
                     InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
   ApplicationContainer sinkApps = sink.Install (wifiStaNodes);
-  sinkptr = StaticCast<PacketSink> (sinkApps.Get (0));
   sinkApps.Start (Seconds (0.0));
   sinkApps.Stop (Seconds (10.0));
-
 
   OnOffHelper onOffHelper ("ns3::TcpSocketFactory", sinkAddress);
   onOffHelper.SetAttribute ("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
@@ -247,20 +212,16 @@ main (int argc, char *argv[])
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
   phyAP.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
-  phyAP.EnablePcap ("AccessPoint", apDevices);
 
   FlowMonitorHelper flowmon;
   Ptr<FlowMonitor> monitor = flowmon.InstallAll();
 
-  Simulator::Schedule (Seconds (1.1), &CalculateThroughput);
+  Ptr<Ipv4> ipv4 = wifiStaNodes.Get (0)->GetObject<Ipv4>();
+  Ipv4InterfaceAddress iaddr = ipv4->GetAddress (1,0);
+  Ipv4Address addri = iaddr.GetLocal ();
+
   Simulator::Stop (Seconds (10.0));
   Simulator::Run ();
-  
-  Ptr<WifiNetDevice> myWifiNetDevice = DynamicCast<WifiNetDevice> (staDevices.Get (0));
-  myWifiNetDevice->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback (&RxDrop));
-  //change
-  //myWifiNetDevice->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback (&RxDrop));
- // Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/Phy/PhyRxDrop", MakeCallback(&RxDrop));
 
   monitor->CheckForLostPackets();
   Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier ());
@@ -273,21 +234,20 @@ main (int argc, char *argv[])
           std::cout<<" Rx Bytes: "<<i->second.rxBytes<<"\n";
           std::cout << "  Tx Packets: " << i->second.txPackets << "\n";
           std::cout << "  Rx Packets: " << i->second.rxPackets << "\n";
-          std::cout << "  lostPackets: " << (i->second.txPackets - i->second.rxPackets) << "\n";
-          std::cout << "  DroppedPackets: " << (dropped) << "\n";
+          //std::cout << "  lostPackets: " << i->second.lostPackets << "\n";
+          if(addri == t.destinationAddress)
+                std::cout << "  DroppedPackets: " << (dropped) << "\n";
+          std::cout << "  Throughput: " << i->second.rxBytes * 8.0 / 9.0 / 1000 / 1000  << " Mbps\n";
   }
  
+  //std::cout << "  DroppedPackets1: " << (dropped1) << "\n";
 
-  double averageThroughput = ((sinkptr->GetTotalRx () * 8) / (1e6  * (double)10));
-
-  std::cout << "\nAverage throughput: " << averageThroughput << " Mbit/s" << std::endl;
-
-  /*if (tracing == true)
+  if (tracing == true)
     {
-      pointToPoint.EnablePcapAll ("third");
-      phy.EnablePcap ("third", staDevices.Get (0));
-      //csma.EnablePcap ("third", csmaDevices.Get (0), true);
-    }*/
+      pointToPoint.EnablePcapAll ("eeosack");
+      phyST.EnablePcap ("Station", staDevices.Get (0));
+      phyAP.EnablePcap ("AccessPoint", apDevices);
+    }
 
   Simulator::Destroy ();
   return 0;
